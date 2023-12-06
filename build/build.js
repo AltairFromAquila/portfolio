ad57b5c21538e87e5029075d059481cd3718f65f
Fabián Arancibia Caprile <fabian.aran.caprile@gmail.com>
2023-12-06 02:47:58 -0300

    GH Pages workflow added

--- build/build.js
+++ build/build.js
@@ -1,6 +1,14 @@
 import * as esbuild from 'esbuild';
 import { getBuildSettings } from './shared.js';
 
-const settings = getBuildSettings('prod', { minify: true });
+try {
+    const settings = getBuildSettings('prod', { minify: true });
+    const result = await esbuild.build(settings);
+    
+    console.log(result);
+    console.log('\x1b[32mBuild done!\x1b[0m');
+} catch (err) {
+    console.error(err);
+}
 
-await esbuild.build(settings);
+process.exit();
